245775f68c7d664a8b5c9c5ddee15267c6ba6067
Rutuja20Sanap <dnyaneshwarsanap293@gmail.com>
2024-08-09 03:09:13 -0930

    added intial code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createRoot} from 'react-dom/client';
+const root = createRoot(document.getElementById('root'));
+const SayHello = ({name})=>{
+  return(<><h2>Hello {name}</h2></>)
+}
+const CakeShop = ()=>{
+return (<div><h1>Dark-Chocolate</h1></div>);
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<>
+<SayHello name ="Rutuja"/>
+<h1>Cake</h1>
+<CakeShop/>
+</>);
