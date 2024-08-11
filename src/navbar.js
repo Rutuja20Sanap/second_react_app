d3aa6f7fab7a779e48c2716b50e4f7c77e9dc221
Rutuja20Sanap <dnyaneshwarsanap293@gmail.com>
2024-08-11 03:02:35 -0930

    delete classname

--- src/navbar.js
+++ src/navbar.js
@@ -6,7 +6,7 @@ import React from 'react'
 function navbar() {
   return (
     <div className="navbar"><span className="heading">Cadburry🍫</span>
-    <div className="navabr-links">
+    <div>
         <a href="/"className="navbar-link">Home</a>
         <a href="/about" className="navbar-link">About</a>
         <a href="/contact" className="navbar-link">Contact</a>
